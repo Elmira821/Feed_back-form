0628965ec378fff0867bc9501c8edd6e9727e3bb
Elmira821 <elmirauyghur@gmail.com>
2024-09-06 15:46:11 +0200

    modified:   Feedback/feedback/__pycache__/__init__.cpython-312.pyc 	modified:   Feedback/feedback/__pycache__/settings.cpython-312.pyc 	modified:   Feedback/feedback/__pycache__/urls.cpython-312.pyc 	modified:   Feedback/feedback/__pycache__/wsgi.cpython-312.pyc 	modified:   Feedback/reviews/__pycache__/__init__.cpython-312.pyc 	modified:   Feedback/reviews/__pycache__/admin.cpython-312.pyc 	modified:   Feedback/reviews/__pycache__/apps.cpython-312.pyc 	new file:   Feedback/reviews/__pycache__/forms.cpython-312.pyc 	modified:   Feedback/reviews/__pycache__/models.cpython-312.pyc 	modified:   Feedback/reviews/__pycache__/urls.cpython-312.pyc 	modified:   Feedback/reviews/__pycache__/views.cpython-312.pyc 	new file:   Feedback/reviews/forms.py 	modified:   Feedback/reviews/migrations/__pycache__/__init__.cpython-312.pyc 	new file:   Feedback/reviews/static/reviews/style.css 	modified:   Feedback/reviews/templates/reviews/review.html 	modified:   Feedback/reviews/views.py

---
+++ Feedback/reviews/forms.py
@@ -0,0 +1,22 @@
+
+
+
+from django import forms
+
+
+class ReviewForm(forms.Form):
+    user_name = forms.CharField(
+        label="Your name", 
+        max_length=100, 
+        error_messages={
+            "required": "Your name must not be empty!",
+            "max_length": "Please enter a shorter name!",
+        }
+    )
+    review_text = forms.CharField(
+        label="Your review", 
+        max_length=1000, 
+        required=False, 
+        widget=forms.Textarea
+    )
+    rating = forms.IntegerField(label="Your rating", min_value=1, max_value=5)
