7c5e82b058be28ee271d4aad9b339b89f6d3b928
Elmira821 <elmirauyghur@gmail.com>
2024-09-10 16:16:33 +0200

    modified:   Feedback/reviews/__pycache__/views.cpython-312.pyc 	modified:   Feedback/reviews/views.py

--- Feedback/reviews/views.py
+++ Feedback/reviews/views.py
@@ -11,22 +11,21 @@ from .models import Review
 
 # Create your views here.
 
+
 class ReviewView(FormView):
     form_class = ReviewForm
     template_name = "reviews/review.html"
-   
-    
+
     # def get (self, request):
     #     form = ReviewForm()
-    
+
     #     return render (request, "reviews/review.html",{
     #         "form" : form
     #     })
-    
-       
+
     def post(self, request):
         form = ReviewForm(request.POST)
-        
+
         if form.is_valid():
             """ print(form.cleaned_data) """
             review = Review(user_name=form.cleaned_data['user_name'],
@@ -34,18 +33,18 @@ class ReviewView(FormView):
                             rating=form.cleaned_data['rating'])
             review.save()
             return HttpResponseRedirect("/thank_you")
-        
-        return render (request, "reviews/review.html",{
-            "form" : form
+
+        return render(request, "reviews/review.html", {
+            "form": form
         })
-        
+
 
 class ThankYouView(TemplateView):
     template_name = "reviews/thank_you.html"
-    
+
     def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        context [" message "] = "This works!"
+        context = super().get_context_data(**kwargs)
+        context[" message "] = "This works!"
         return context
 
 
@@ -53,28 +52,27 @@ class ReviewListView(ListView):
     template_name = "reviews/review_list.html"
     model = Review
     context_object_name = "reviews"
-    
+
     #  def get_context_data(self, **kwargs):
     #     context=  super().get_context_data(**kwargs)
     #     reviews = Review.objects.all()
     #     context["reviews"] = reviews
-    #     return context """                            This is when we used Template View 
-    #  we can add extra methods here, example:  
-          
+    #     return context """                            This is when we used Template View
+    #  we can add extra methods here, example:
+
     def get_queryset(self):
-        base_query= super().get_queryset()
-        data=base_query.filter(rating__gt=2)
+        base_query = super().get_queryset()
+        data = base_query.filter(rating__gt=2)
         return data
-    
+
 
 class ReviewDetailView(DetailView):
     template_name = "reviews/review_detail.html"
     model = Review
-    
+
     # def get_context_data(self, **kwargs):
     #     context=  super().get_context_data(**kwargs)
     #     review_id = kwargs["id"]
     #     selected_review = Review.objects.get(pk=review_id)
     #     context["review"] = selected_review
-    #     return context                                            This is when we used Template View 
-    
+    #     return context                                            This is when we used Template View
