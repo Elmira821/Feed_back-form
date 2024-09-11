fc42a37bb961fb2528a6e2f239a28e71d3d960b3
Elmira821 <elmirauyghur@gmail.com>
2024-09-11 10:56:15 +0200

    media configurations

--- Feedback/profiles/views.py
+++ Feedback/profiles/views.py
@@ -1,21 +1,48 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+# from .forms import ProfileForm
+from .models import UserProfile
 
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 # Create your views here.
 
-def store_file(file):
-    with open("temp/image.png","wb+") as dest:
-        for chunk in file.chunks():
-            dest.write(chunk)
-    
-    
-    
-class CreateProfileView(View):
-    def get(self, request):
-        return render(request, "profiles/create_profile.html")
+class CreateProfileView(CreateView):
+    template_name = "profiles/create_profile.html"
+    model = UserProfile
+    fields = "__all__"
+    success_url = "/profiles/"
 
-    def post(self, request):
-        store_file (request.FILES["image"])   #this image name is the same as in the create_profile.html, <input type="file" name="image"/>
-        return HttpResponseRedirect("/profiles")
+class ProfilesView(ListView):
+    template_name = "profiles/user_profiles.html"
+    model = UserProfile
+    context_object_name = "profiles"
+    
+
+# def store_file(file):
+#     with open("temp/image.png","wb+") as dest:
+#         for chunk in file.chunks():
+#             dest.write(chunk)
+    
+# class CreateProfileView(View):
+#     def get(self, request):
+#         form = ProfileForm()
+#         return render(request, "profiles/create_profile.html", {
+#             "form":form    #this "form" allows us to replace the whole input to {{form}} inside create_profile.html
+#         })
+
+#     def post(self, request):
+#         submitted_form = ProfileForm(request.POST, request.FILES)
+        
+#         if submitted_form.is_valid():
+#             profile = UserProfile(image = request.FILES["user_image"])
+#             profile.save()
+#             return HttpResponseRedirect("/profiles")
+        
+#         return render(request, "profiles/create_profile.html", {
+#             "form":submitted_form
+#         })
+        
+        
         
